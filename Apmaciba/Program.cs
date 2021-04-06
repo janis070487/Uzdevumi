126e0f2862073983d3a0f5dd0b74cfe4fe22cafe
janis070487 <janis070487@gmail.com>
2021-04-06 23:56:43 +0300

    V1.0

---
+++ Apmaciba/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Apmaciba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] arr;
+			int size = 0;
+			while (!(size > 0))
+			{
+				Console.WriteLine("Ievadi elementu skaitu");
+				try
+				{
+					size = int.Parse(Console.ReadLine());
+				}
+				catch (Exception)
+				{
+					Console.WriteLine(" Ievadiet vesalu pozitīvu skaitli");
+				}
+			}
+			arr = new int[size];
+			Console.WriteLine();
+			for (int i = 0; i < arr.Length; i++)
+			{
+				bool status = true;
+				while (status)
+				{
+					Console.WriteLine("ievadi " + (i + 1) + ". elementu");
+					try
+					{
+						arr[i] = int.Parse(Console.ReadLine());
+						status = false;
+					}
+					catch (Exception)
+					{
+						Console.WriteLine(" Ievadi tikai pozitīvus veselus skaitlus");
+						//status = false;
+					}
+					//
+					
+				}
+			}
+			Console.WriteLine("___________REZULTĀTS_______");
+			Console.WriteLine();
+			int summ = 0;
+			int min = arr[0];
+
+			for (int ii = arr.Length; ii > 0; ii--)
+			{
+				Console.WriteLine(arr[ii - 1]);
+				if(arr[ii - 1] % 2 == 0)
+                {
+					summ = summ + arr[ii - 1];
+                }
+
+                if (arr[ii - 1] < min)
+                {
+					min = arr[ii - 1];
+                }
+			}
+
+			Console.WriteLine("Pāra skaitļu summa masīvā = " + summ);
+			Console.WriteLine();
+			Console.WriteLine("Mazākais elements masīvā = " + min);
+			Console.ReadLine();
+			//_________________________________________________________________
+		}
+    }
+}
